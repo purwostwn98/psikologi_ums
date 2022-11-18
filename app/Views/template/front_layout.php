731dc63842d1d92366f321d713838c2572520831
falah <masterwarefun@gmail.com>
2022-11-18 21:44:23 +0700

    fix flag img

--- app/Views/template/front_layout.php
+++ app/Views/template/front_layout.php
@@ -198,12 +198,12 @@ $session = \Config\Services::session();
 
                     <li id="toggle-language" class="dropdown">
                         <a id="a_dropdown" href="#">
-                            <img src="<?= base_url(); ?>/global/in.gif" alt="" srcset="">
+                            <img src="<?= base_url(); ?>/depan/assets/img/in.gif" alt="" srcset="">
                             &nbsp;<span>Indonesia</span> <i class="bi bi-chevron-down dropdown-indicator"></i>
                         </a>
                         <ul>
-                            <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
-                            <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
+                            <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
+                            <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
                         </ul>
                     </li>
                 </ul>
@@ -603,21 +603,21 @@ $session = \Config\Services::session();
     const lang_url = `<?= $locale ?>`
     console.log(lang_url)
     const id = `<a href="#">
-                        <img src="<?= base_url(); ?>/global/in.gif" alt="" srcset=""> 
+                        <img src="<?= base_url(); ?>/depan/assets/img/in.gif" alt="" srcset=""> 
                         &nbsp;<span>Indonesia</span> <i class="bi bi-chevron-down dropdown-indicator"></i>
                     </a>
                     <ul>
-                        <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
-                        <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
+                        <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
+                        <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
                     </ul>`
 
     const en = `<a href="#">
-                    <img src="<?= base_url(); ?>/global/en.gif" alt="" srcset=""> 
+                    <img src="<?= base_url(); ?>/depan/assets/img/en.gif" alt="" srcset=""> 
                     &nbsp;<span>English</span> <i class="bi bi-chevron-down dropdown-indicator"></i>
                 </a>
                 <ul>
-                    <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
-                    <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/global/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
+                    <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/in.gif" style="margin-right:5px" alt="" srcset="">Indonesia</span></a></li>
+                    <li><a href="#"> <span class="option-lang"><img src="<?= base_url(); ?>/depan/assets/img/en.gif" style="margin-right:5px" alt="" srcset="">English</span></a></li>
                 </ul>`
 
     document.cookie = `language=${lang_url}`;
